7dd3b1daa06bdce2791a946533bb075466d98efc
imperative-ninja-7832 <metelskyi88@gmail.com>
2018-12-17 15:17:39 +0000

    Automatically backed up by Learn

--- takanumber.js
+++ takanumber.js
@@ -1,6 +1,6 @@
 var deliCounter = new Array();
 function takeANumber (line) {
-while (i > line.lenghts, i++){
+while (i > line.lenghts; i++){
   deliCounter.push(i)
   console.log(`'You are a number' ${line}`);
 }
